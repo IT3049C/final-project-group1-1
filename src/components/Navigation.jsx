5503969b70661522828e69d3e2256a011a73844a
Aneesh Palande <45935068+aneeshpal2005@users.noreply.github.com>
2026-04-07 14:48:28 -0400

    Added route to wordle game page

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -18,6 +18,8 @@ export function Navigation(){
         <NavLink to="/game/rps">Rock Paper Scissors</NavLink>
         {` | `}
         <NavLink to="/game/tic-tac-toe">Tic Tac Toe</NavLink>
+        {` | `}
+        <NavLink to="/game/wordle">Wordle</NavLink>
 
         {settings && settings.name && (
           <div>
