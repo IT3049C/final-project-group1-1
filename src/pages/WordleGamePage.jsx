508d0a69d22621eb1a61f3a29403441af3d98043
Aneesh Palande <45935068+aneeshpal2005@users.noreply.github.com>
2026-04-07 14:45:40 -0400

    Added route to wordle game page

---
+++ src/pages/WordleGamePage.jsx
@@ -0,0 +1,8 @@
+export function WordleGamePage(){
+    return(
+        <div className="wordle-game-page">
+            <h2>Wordle Game</h2>
+            <p>Wordle Game Placeholder</p>
+        </div>
+    );
+}
